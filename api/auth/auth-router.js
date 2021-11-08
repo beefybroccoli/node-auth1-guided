1f19bed1d93a7878d17504c005a1db0d3ba9bf74
Ladrillo <gabriel@cabrejas.net>
2021-11-08 18:23:25 +0100

    wip

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,7 +1,12 @@
 const router = require('express').Router()
+const bcrypt = require('bcryptjs')
 
 router.post('/register', async (req, res, next) => {
-  res.json('register endpoint wired!')
+  try {
+    
+  } catch (err) {
+    next(err)
+  }
 })
 
 router.post('/login', async (req, res, next) => {
