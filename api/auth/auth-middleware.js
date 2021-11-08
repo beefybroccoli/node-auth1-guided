2f581e04607c461a964b9c02a644176c73672271
Ladrillo <gabriel@cabrejas.net>
2021-11-08 19:28:20 +0100

    wip

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,4 +1,12 @@
 function onlyAuthed(req, res, next) {
   // only allow req to proceed if there is a session
-  if (req.session.user)
+  if (req.session.user) {
+    next()
+  } else {
+    next({ status: 403, message: 'not for you!' })
+  }
+}
+
+module.exports = {
+  onlyAuthed,
 }
