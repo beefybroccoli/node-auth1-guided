2164547e31dc5919455b9f16cd1b893832997174
Ladrillo <gabriel@cabrejas.net>
2021-11-08 18:25:46 +0100

    wip

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -3,7 +3,8 @@ const bcrypt = require('bcryptjs')
 
 router.post('/register', async (req, res, next) => {
   try {
-    
+    const { username, password } = req.body
+    const newUser = { username, password }
   } catch (err) {
     next(err)
   }
