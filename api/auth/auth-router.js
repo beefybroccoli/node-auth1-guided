dccd3633fb59c74d2c8682d29c4a37e737b7e104
Ladrillo <gabriel@cabrejas.net>
2021-11-08 18:29:41 +0100

    wip

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -5,6 +5,7 @@ const User = require('../users/users-model')
 router.post('/register', async (req, res, next) => {
   try {
     const { username, password } = req.body
+    const hash = bcrypt.hashSync(password, )
     const newUser = { username, password }
     const result = await User.add(newUser)
     res.status(201).json(result)
